280ad8615804baa28c193509feff670eb8cbe804
Mohammad Abdul Khadar <abdul.khadar.tech@gmail.com>
2026-03-15 20:46:19 +0530

    Update analytics.py

--- pages/analytics.py
+++ pages/analytics.py
@@ -27,8 +27,8 @@ with st.sidebar:
     st.write("")
     if st.sidebar.button("Back to Prediction"):
         st.switch_page("pages/app.py")
-    if st.sidebar.button("Home"):
-        st.switch_page("Home.py")
+    if st.sidebar.button("home"):
+        st.switch_page("home.py")
 
 mode = st.session_state.dark_mode
 ACCENT = "#E8B84B"
